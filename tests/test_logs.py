4bafea5d7dbddfe26190ab9611bb40cd072c67d2
Vince Dority <vncdority@gmail.com>
2025-08-04 10:23:27 -0400

    Current logging functions tested:ERROR,CRITICAL, and WARNING are now set to stderr instead of stdout. INFO and DEBUG levels are still streamed throgh stdout

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -1,3 +1,5 @@
+import collections
+
 import logs
 import logging
 import pytest
@@ -24,5 +26,67 @@ def test_info_level_logs(caplog):
     assert "ERROR level test message" in caplog.text
     assert "CRITICAL level test message" in caplog.text
     assert "DEBUG level test message" not in caplog.text
+def test_debug_msg(caplog):
+    caplog.set_level(logging.DEBUG)
+    debug_logger = logs.Logs(name="Debug_test",level=logging.DEBUG)
+    debug_logger._logger.addHandler(caplog.handler)
+    debug_logger.debug('Debug test message')
+    debug_logger._logger.removeHandler(caplog.handler)
+    debug_logger.close()
+    assert "Debug test message" in caplog.text
+def test_warning_msg(caplog):
+    """
+    Test for WARNING,ERROR,CRITICAL level logs
+    """
+    caplog.set_level(logging.WARNING)
+    warning_level_test = logs.Logs(name='Warning level test',level=logging.WARNING)
+    warning_level_test._logger.addHandler(caplog.handler)
+    warning_level_test.info('INFO msg should be omitted')
+    warning_level_test.debug('DEBUG msg should be omitted')
+    warning_level_test.warning('Warning test functional')
+    warning_level_test.critical('CRITICAL test functional')
+    warning_level_test.error('ERROR test functional')
+    warning_level_test._logger.removeHandler(caplog.handler)
+    warning_level_test.close()
+    assert 'INFO msg should be omitted' not in caplog.text
+    assert 'DEBUG msg should be omitted' not in caplog.text
+    assert 'Warning test functional' in caplog.text
+    assert 'CRITICAL test functional' in caplog.text
+    assert 'ERROR test functional' in caplog.text
+
+def test_error_msg(caplog):
+    caplog.set_level(logging.ERROR)
+    error_level_test = logs.Logs(name='Error level test',level=logging.ERROR)
+    error_level_test._logger.addHandler(caplog.handler)
+    error_level_test.info('INFO msg should be omitted')
+    error_level_test.debug('DEBUG msg should be omitted')
+    error_level_test.warning('WARNING msg should be omitted')
+    error_level_test.error('ERROR test functional')
+    error_level_test.critical('CRITICAL test functional')
+    error_level_test._logger.removeHandler(caplog.handler)
+    error_level_test.close()
+    assert 'INFO msg should be omitted' not in caplog.text
+    assert 'DEBUG msg should be omitted' not in caplog.text
+    assert 'WARNING msg should be omitted' not in caplog.text
+    assert 'ERROR test functional' in caplog.text
+    assert 'CRITICAL test functional' in caplog.text
+def test_critical_msg(caplog):
+    caplog.set_level(logging.CRITICAL)
+    critical_level_test = logs.Logs(name='CRITICAL level test', level= logging.CRITICAL)
+    critical_level_test._logger.addHandler(caplog.handler)
+    critical_level_test.info('INFO msg should be omitted')
+    critical_level_test.debug('DEBUG msg should be omitted')
+    critical_level_test.warning('WARNING msg should be omitted')
+    critical_level_test.error('ERROR msg should be omitted')
+    critical_level_test.critical('CRITICAL test functional')
+    critical_level_test._logger.removeHandler(caplog.handler)
+    critical_level_test.close()
+    assert 'INFO msg should be omitted' not in caplog.text
+    assert 'DEBUG msg should be omitted' not in caplog.text
+    assert 'WARNING msg should be omitted' not in caplog.text
+    assert 'ERROR msg should be omitted' not in caplog.text
+    assert 'CRITICAL test functional' in caplog.text
+
+
 
 
