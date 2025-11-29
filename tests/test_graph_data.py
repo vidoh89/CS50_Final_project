3c76464a3ab6f41b0451b08203f8970e06a626fc
Vince Dority <vncdority@gmail.com>
2025-11-29 04:28:20 -0500

    Updates made to pytest for graphing module. Test have been written to ensure certain values are updated correctly for base graphing elements and xaxis

--- tests/test_graph_data.py
+++ tests/test_graph_data.py
@@ -33,7 +33,7 @@ def test_graph_initialization(grapher):
     :return: An instance of Graph_For_Data
     """
     assert isinstance(grapher,Graph_For_Data)
-def test_graph_generator_happy_path(grapher, valid_df):
+def test_graph_generator_traces(grapher, valid_df):
     """
     Test Figure
     :param grapher: Instance of the Graph_For_Data
@@ -52,13 +52,44 @@ def test_graph_generator_happy_path(grapher, valid_df):
     assert list(trace_1.x) == list(valid_df.index)
     assert trace_1.line.color =='cyan'
     assert trace_1.line.width == 2
+    assert trace_1.textfont.size== 4
 
 
     trace_2 = test_fig.data[1]
-
+    assert list(trace_2.x) == list(valid_df.index)
     assert trace_2.name == 'Growth Rate(%)'
     assert trace_2.mode == 'lines+markers'
     assert trace_2.yaxis == 'y2'
+
+def test_graph_layout(grapher,valid_df):
+    """
+    Test that plotly updates the graph object with correct parameter values
+    :param grapher: Figure object
+    :param valid_df: Test data for testing environment
+    :return:
+    """
+    test_fig = grapher.graph_generator(valid_df,fig_title="TEST_DATA")
+    test_layout = test_fig.layout
+    assert test_layout.font.size == 9.5
+    assert test_layout.hovermode == 'x unified'
+    assert test_layout.title.text == 'US Real GDP and Quarterly Growth Rate'
+    assert test_layout.title.x== 0.5
+    assert test_layout.title.y == 0.75
+    assert test_layout.legend.orientation == 'h'
+    assert test_layout.legend.yanchor == 'bottom'
+    assert test_layout.legend.xanchor == 'center'
+    assert test_layout.legend.y == 1.00
+    assert test_layout.legend.x == 0.5
+    assert test_layout.legend.bgcolor == 'rgba(0,0,0,0)'
+def test_graph_layout_xaxis(grapher,valid_df):
+    test_graph = grapher.graph_generator(valid_df,fig_title='TEST_DATA_xaxis')
+    test_layout = test_graph.layout.xaxis
+    assert test_layout.title.text == 'Date (Quarterly)'
+    assert test_layout.type =='date'
+    assert test_layout.tickformat=='%Y Q%q'
+    assert test_layout.tickfont.size ==6
+
+
 
 
 
